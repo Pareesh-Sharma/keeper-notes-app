e54e365ae5d1aabd6595cad94281ab62afce0418
Pareesh Sharma <130954142+Pareesh-Sharma@users.noreply.github.com>
2025-05-22 15:23:58 +0530

    Add files via upload

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// In VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
